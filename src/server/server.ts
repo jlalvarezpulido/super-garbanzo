5d02047c658ff53bed3e4b819fe263ea46ecb6b0
Jose Alvarez Pulido <jlalvarezpulido@gmail.com>
2023-03-27 22:34:15 -0700

    added EJS for templating

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,11 +1,9 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
 
 server.use(express.static("dist"));
 
-server.listen("8080", ()=>{
-    console.info(
-        "Express server is listening at port 8080"
-    );
+server.listen("8080", () => {
+  console.info("Express server is listening at port 8080");
 });
