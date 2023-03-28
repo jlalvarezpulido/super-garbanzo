6490b418978b5133a68bdbfb23a45513cf4f6d38
Jose Alvarez Pulido <jlalvarezpulido@gmail.com>
2023-03-27 22:45:04 -0700

    Added EJS for templating

--- src/server/server.ts
+++ src/server/server.ts
@@ -4,6 +4,12 @@ const server = express();
 
 server.use(express.static("dist"));
 
+server.set("view engine", "ejs");
+
+server.use("/", (req, res) => {
+  res.render("index");
+});
+
 server.listen("8080", () => {
   console.info("Express server is listening at port 8080");
 });
